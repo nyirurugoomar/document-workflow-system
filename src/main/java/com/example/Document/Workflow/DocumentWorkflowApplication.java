b25ff5019c6ca9758667af6a680e97b20fb1937c
nyirurugo omar <nyirurugoomar@gmail.com>
2025-12-30 17:20:16 +0200

    Initial commit

---
+++ src/main/java/com/example/Document/Workflow/DocumentWorkflowApplication.java
@@ -0,0 +1,13 @@
+package com.example.Document.Workflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentWorkflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentWorkflowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Document/Workflow/DocumentWorkflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Document.Workflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentWorkflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
